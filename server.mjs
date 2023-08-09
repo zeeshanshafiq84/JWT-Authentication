7d319387a7d94e7aaa736652c9c57fd7a5b5ccfc
zeeshan <zeeshanshafiq84>
2023-08-09 15:35:24 -0700

    =import jsonwebtoken

--- server.mjs
+++ server.mjs
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config'
 const __dirname = path.resolve();
 
 import authRouter from './routes/auth.mjs'
 import commentRouter from './routes/comment.mjs'
 import feedRouter from './routes/feed.mjs'
 import postRouter from './routes/post.mjs'
+import cookieParser from 'cookie-parser'
+import { decode } from 'punycode';
+
 
 
 
@@ -16,6 +21,7 @@ import postRouter from './routes/post.mjs'
 
 const app = express();
 app.use(express.json()); // body parser
+app.use(cookieParser()); // body parser
 // app.use(cors())
 
 // /api/v1/login
@@ -23,12 +29,27 @@ app.use("/api/v1", authRouter)
 
 
 app.use((req, res, next) => {   //JWT
-    let token = "valid"
-    if (token === "valid") {
+    console.log("cookies:", req.cookies);
+
+    const token = req.cookies.token;
+    
+    try{
+        const decode = jwt.verify(token, process.env.SECRET);
+        console.log("decoded:", decoded);
+
+        req.body.decoded = {
+            firstName: decoded.firstName,
+            lastName: decoded.lastName,
+            email: decoded.email,
+            isAdmin: decoded.isAdmin,
+        };
+
         next();
-    } else {
-        res.send({ message: "invalid token" })
-  }
+    
+    } catch (err) {
+        res.status(401).send({ message: "invalid token"})
+    }
+
 })
 
 
