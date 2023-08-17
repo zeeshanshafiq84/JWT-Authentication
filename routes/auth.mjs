80334870424307d0b4d64a5d1ef7812c7776fda4
zeeshan <zeeshanshafiq84>
2023-08-17 07:58:48 -0700

    =some changes in auth.mjs

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -10,6 +10,62 @@ import {
 
 
 const userCollection = client.db("cruddb").collection("users");
+router.post('/signup', async (req, res, next) => {
+
+    if (
+        !req.body?.firstName
+        || !req.body?.lastName // family name, sur name
+        || !req.body?.email
+        || !req.body?.password
+    ) {
+        res.status(403);
+        res.send(`required parameters missing, 
+        example request body:
+        {
+            firstName: "some firstName",
+            lastName: "some lastName",
+            email: "some@email.com",
+            password: "some$password",
+        } `);
+        return;
+    }
+
+    req.body.email = req.body.email.toLowerCase();
+    // TODO: validate email
+
+
+    try {
+        let result = await userCollection.findOne({ email: req.body.email });
+        console.log("result: ", result);
+
+        if (!result) { // user not found
+
+            const passwordHash = await stringToHash(req.body.password);
+
+            const insertResponse = await userCollection.insertOne({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: passwordHash,
+                createdOn: new Date()
+            });
+            console.log("insertResponse: ", insertResponse);
+
+            res.send({ message: 'Signup successful' });
+
+        } else { // user already exists
+            res.status(403).send({
+                message: "user already exist with this email"
+            });
+        }
+
+    } catch (e) {
+        console.log("error getting data mongodb: ", e);
+        res.status(500).send('server error, please try later');
+    }
+})
+
+
 
 router.post('/login', async (req, res, next) => {
 
@@ -76,60 +132,5 @@ router.post('/login', async (req, res, next) => {
         res.status(500).send('server error, please try later');
     }
 })
-router.post('/signup', async (req, res, next) => {
-
-    if (
-        !req.body?.firstName
-        || !req.body?.lastName // family name, sur name
-        || !req.body?.email
-        || !req.body?.password
-    ) {
-        res.status(403);
-        res.send(`required parameters missing, 
-        example request body:
-        {
-            firstName: "some firstName",
-            lastName: "some lastName",
-            email: "some@email.com",
-            password: "some$password",
-        } `);
-        return;
-    }
-
-    req.body.email = req.body.email.toLowerCase();
-    // TODO: validate email
-
-
-    try {
-        let result = await userCollection.findOne({ email: req.body.email });
-        console.log("result: ", result);
-
-        if (!result) { // user not found
-
-            const passwordHash = await stringToHash(req.body.password);
-
-            const insertResponse = await userCollection.insertOne({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: passwordHash,
-                createdOn: new Date()
-            });
-            console.log("insertResponse: ", insertResponse);
-
-            res.send({ message: 'Signup successful' });
-
-        } else { // user already exists
-            res.status(403).send({
-                message: "user already exist with this email"
-            });
-        }
-
-    } catch (e) {
-        console.log("error getting data mongodb: ", e);
-        res.status(500).send('server error, please try later');
-    }
-})
-
 
 export default router
--- server.mjs
+++ server.mjs
@@ -28,7 +28,7 @@ app.use(cookieParser()); // body parser
 // /api/v1/login
 app.use("/api/v1", authRouter)
 
-app.use("/static", express.static(path.join(__dirname, 'static')))
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
